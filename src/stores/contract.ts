74cf8e957ef4ceb57989e297b43ccdf3e6c9c932
Joao Koritar <jakoritarleite@gmail.com>
2021-10-30 14:29:25 -0300

    feat: update contract artifacts and contract address

--- src/stores/contract.ts
+++ src/stores/contract.ts
@@ -2,7 +2,7 @@ import { readable } from 'svelte/store';
 import abi from '@app/artifacts/WavePortal.json';
 
 export const contractAddress = readable(
-  '0x4a8ae3253b413505e02f33c33745958998ba14f0'
+  '0x9e185e76b04e34a8fC17Ba9b2B21f79445a83731'
 );
 
 export const contractABI = readable(abi.abi);
